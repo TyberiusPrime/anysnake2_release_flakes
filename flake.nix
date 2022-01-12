56f3d4a716889d83da444811c6ee6f8e6d352b28
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2022-01-12 11:47:02 +0100

    1.0.8

--- flake.nix
+++ flake.nix
@@ -15,11 +15,11 @@
     in {
       defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation rec {
         name = "anysnake";
-        version = "1.0.7";
+        version = "1.0.8";
         src = pkgs.fetchurl {
           url =
             "https://github.com/TyberiusPrime/anysnake2/releases/download/${version}/anysnake2_${version}_x86_64-unknown-linux-musl.tar.gz";
-          sha256 = "sha256-aNdGpaIKIBWeW1U6DMSIpMHR+UbepRX633APiT6wpv8=";
+          sha256 = "sha256-FKEd140XTSG9GH+kPOULs14SoHIOWtW9oOSnPXGRxzg=";
         };
         sourceRoot = ".";
         installPhase = ''
